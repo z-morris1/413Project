e845c342946a205e5a20c4204b0e585519c0d4db
Quang Le <qmle@iastate.edu>
2023-11-12 15:24:15 -0600

    Connect to ChatGPT API

---
+++ APICall.py
@@ -0,0 +1,26 @@
+import requests
+
+def send_prompt_to_chatgpt(prompt, api_key):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": "gpt-3.5-turbo",  # or another model name if you prefer
+        "messages": [{"role": "user", "content": prompt}]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.text}
+
+# Replace 'your_api_key_here' with your actual OpenAI API key
+api_key = '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'
+prompt = "Say Hello to me"
+
+response = send_prompt_to_chatgpt(prompt, api_key)
+print(response)
+
