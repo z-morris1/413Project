65463e2e6b31b4d045860c2ad4dd13d83f9328a9
z-morris1 <149324551+z-morris1@users.noreply.github.com>
2023-12-07 09:06:26 -0600

    Update APICall.py

--- APICall.py
+++ APICall.py
@@ -41,7 +41,7 @@ with open("prompt.txt", 'r') as file:
 response = send_prompt_to_chatgpt(prompt, api_key)
 
 #Zach
-response = {'id': 'chatcmpl-8Px7H7LLAH6etALiylt9kIsavMnmX', 'object': 'chat.completion', 'created': 1701196135, 'model': 'gpt-3.5-turbo-0613', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': 'Input:\ninput_string: "abcd"\nsubstring: "bc"\n\nInput:\ninput_string: "hello"\nsubstring: "lo"\n\nInput:\ninput_string: "123"\nsubstring: "45"'}, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 203, 'completion_tokens': 39, 'total_tokens': 242}}
+# response = {'id': 'chatcmpl-8Px7H7LLAH6etALiylt9kIsavMnmX', 'object': 'chat.completion', 'created': 1701196135, 'model': 'gpt-3.5-turbo-0613', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': 'Input:\ninput_string: "abcd"\nsubstring: "bc"\n\nInput:\ninput_string: "hello"\nsubstring: "lo"\n\nInput:\ninput_string: "123"\nsubstring: "45"'}, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 203, 'completion_tokens': 39, 'total_tokens': 242}}
 # variable1 = input("name of the first variable: ")
 # variable2 = input("name of the second variable: ")
 # Extracting input strings and substrings
