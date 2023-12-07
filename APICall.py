6e10358523406f55e3ea5e3b850f5c4e2c61da36
RichardIcecube <66053594+RichardIcecube@users.noreply.github.com>
2023-12-07 10:39:08 -0600

    Update APICall.py

--- APICall.py
+++ APICall.py
@@ -40,8 +40,6 @@ with open("prompt.txt", 'r') as file:
 
 response = send_prompt_to_chatgpt(prompt, api_key)
 
-print(response)
-
 #Zach
 # response = {'id': 'chatcmpl-8Px7H7LLAH6etALiylt9kIsavMnmX', 'object': 'chat.completion', 'created': 1701196135, 'model': 'gpt-3.5-turbo-0613', 'choices': [{'index': 0, 'message': {'role': 'assistant', 'content': 'Input:\ninput_string: "abcd"\nsubstring: "bc"\n\nInput:\ninput_string: "hello"\nsubstring: "lo"\n\nInput:\ninput_string: "123"\nsubstring: "45"'}, 'finish_reason': 'stop'}], 'usage': {'prompt_tokens': 203, 'completion_tokens': 39, 'total_tokens': 242}}
 # variable1 = input("name of the first variable: ")
